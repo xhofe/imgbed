8a5b10983f03e16a1af3afe79f946fa9d0e72ca9
微凉 <i@nn.ci>
2021-07-06 00:14:53 +0800

    Update transit.ts

--- api/transit.ts
+++ api/transit.ts
@@ -8,7 +8,7 @@ const allowCors = (fn:handler) => async (req: VercelRequest, res: VercelResponse
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   // res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin||'*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
