c6665ad3f3f3349e831df080227162ce11e52c2d
微凉 <i@nn.ci>
2021-07-17 21:31:16 +0800

    粘贴上传

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,32 @@
     <el-main>
       <div class="main">
         <div id="radios">
-          <div class="radio" v-for="api_option in api_options" :key="api_option.path">
+          <div
+            class="radio"
+            v-for="api_option in api_options"
+            :key="api_option.path"
+          >
             <el-radio :label="api_option.path" v-model="choose_api">
-              <el-tag 
+              <el-tag
                 :type="api_option.api.transit ? 'danger' : 'success'"
                 effect="plain"
-              >{{
-                api_option.api.name
-              }}</el-tag>
+                >{{ api_option.api.name }}</el-tag
+              >
             </el-radio>
           </div>
         </div>
         <el-upload
           drag
           action="#"
+          :show-file-list="false"
           :http-request="httpRequest"
           ref="uploader"
           :auto-upload="true"
-          :disabled="choose_api==''"
+          :disabled="choose_api == ''||uploading"
           @click="clickUpload"
         >
           <i class="el-icon-upload"></i>
-          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__text">粘贴/将文件拖到此处，或<em>点击上传</em></div>
         </el-upload>
         <url-show
           v-show="url !== ''"
@@ -38,13 +42,15 @@
       </div>
     </el-main>
     <div class="footer">
-      <el-link href="https://github.com/Xhofe" type="success" target="_blank">Github</el-link>
+      <el-link href="https://github.com/Xhofe" type="success" target="_blank"
+        >Github</el-link
+      >
     </div>
   </el-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, onBeforeUnmount, onMounted, ref } from "vue";
 import ImgApi from "./img_api";
 import { ElMessage } from "element-plus";
 import upload from "./utils/upload";
@@ -61,6 +67,7 @@ export default defineComponent({
     UrlShow,
   },
   setup() {
+    const uploading = ref(false)
     const apis = import.meta.globEager("./apis/*.ts");
     const api_options = ref<Option[]>([]);
     const choose_api = ref("");
@@ -69,12 +76,13 @@ export default defineComponent({
     const uploader = ref<any>(null);
     for (const path in apis) {
       const api = apis[path].default as ImgApi;
-      api_options.value.push({ path: path, api:api });
+      api_options.value.push({ path: path, api: api });
     }
     const httpRequest = (param: any) => {
       const file = param.file;
+      uploading.value = true;
       upload(apis[choose_api.value].default as ImgApi, file).then((res) => {
-        console.log(res);
+        uploading.value = false;
         if (!res.img_url || res.err_msg) {
           ElMessage.error(res.err_msg);
           param.onError();
@@ -85,13 +93,52 @@ export default defineComponent({
         name.value = file.name;
       });
     };
-    const clickUpload = ()=>{
+    const clickUpload = () => {
       if (!choose_api.value) {
         ElMessage.warning("请先选择一个接口");
         return;
       }
-      uploader.value!.clearFiles();
-    }
+      if(uploading.value){
+        ElMessage.warning("正在上传中, 请稍后...");
+        return
+      }
+    };
+    const pasteUpload = async (e: any) => {
+      if (!choose_api.value) {
+        ElMessage.warning("请先选择一个接口");
+        return;
+      }
+      if(uploading.value){
+        ElMessage.warning("正在上传中, 请稍后...");
+      }
+      const items: any = e.clipboardData!.items;
+      for (const item of items) {
+        if (item.type.indexOf("image") !== -1) {
+          const file = item.getAsFile();
+          uploading.value = true;
+          const res = await upload(
+            apis[choose_api.value].default as ImgApi,
+            file
+          );
+          uploading.value = false;
+          if (!res.img_url || res.err_msg) {
+            ElMessage.error(res.err_msg);
+            return;
+          }
+          url.value = res.img_url;
+          name.value = file.name;
+          return;
+        }
+      }
+      ElMessage.warning("请粘贴图片文件");
+    };
+
+    onMounted(() => {
+      document.addEventListener("paste", pasteUpload);
+    });
+    onBeforeUnmount(() => {
+      document.removeEventListener("paste", pasteUpload);
+    });
     return {
       api_options,
       choose_api,
@@ -100,6 +147,7 @@ export default defineComponent({
       httpRequest,
       uploader,
       clickUpload,
+      uploading,
     };
   },
 });
@@ -132,7 +180,7 @@ export default defineComponent({
   gap: 5px;
   justify-items: start;
 }
-.el-upload{
+.el-upload {
   margin: 10px 0;
 }
 .el-upload-dragger {
@@ -143,7 +191,7 @@ export default defineComponent({
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
 }
-.el-tag{
+.el-tag {
   min-width: 74px;
 }
 </style>
