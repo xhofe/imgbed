4c5b074932724c16f5a63385aa1c96a9f2a0f336
微凉 <i@nn.ci>
2021-07-19 15:44:00 +0800

    添加四个接口

---
+++ src/apis/tuchuangs.ts
@@ -0,0 +1,16 @@
+import ImgApi from "../img_api";
+
+const api: ImgApi = {
+  name: '小波图床',
+  transit: false,
+  url: 'https://www.tuchuangs.com/upload/localhost',
+  field_name: 'file',
+  resp_type: 'json',
+  url_field: ['url'],
+  code_field: ['code'],
+  success_code: 200,
+  max_size: 0,
+  extensions: [],
+}
+
+export default api
